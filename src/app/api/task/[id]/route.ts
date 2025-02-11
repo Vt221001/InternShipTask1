6de35e233bf082c7f56db9ed8c0ac54b6ab539ff
vedansh tiwari <vt2855028@gmail.com>
2025-02-11 11:08:41 +0530

    Initial commit

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -2,9 +2,32 @@ import { connect } from "@/Db/db";
 import { Task } from "@/model/taskModel";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Record<string, string> }
+) {
+  const { id } = params;
+
+  try {
+    await connect();
+    const task = await Task.findById(id);
+
+    if (!task) {
+      return NextResponse.json({ message: "Task not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(task, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error fetching task", error },
+      { status: 500 }
+    );
+  }
+}
+
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Record<string, string> }
 ) {
   const { id } = params;
   const { title, description, dueDate, completed } = await req.json();
@@ -22,31 +45,10 @@ export async function PUT(
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Record<string, string> }
 ) {
   const { id } = params;
   await connect();
   await Task.findByIdAndDelete(id);
   return NextResponse.json({ message: "Task Deleted" }, { status: 200 });
-}
-
-
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  await connect();
-
-  try {
-    const task = await Task.findById(params.id);
-    if (!task) {
-      return NextResponse.json({ message: "Task not found" }, { status: 404 });
-    }
-    return NextResponse.json(task, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Error fetching task", error },
-      { status: 500 }
-    );
-  }
 }
